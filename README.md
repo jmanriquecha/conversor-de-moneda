071b9cdcfef2de9001530014012d9916571df647
Jorge Manrique <georgemanc.28@gmail.com>
2024-11-19 17:57:29 -0500

    Se mejora interacción con el usuario a través de la terminal, se documenta código

--- README.md
+++ README.md
@@ -1,2 +1,62 @@
 # App Conversor de Moneda
-Practicando con Java: Challenge Conversor de Monedas
+
+Este es un proyecto en Java que permite convertir entre diferentes monedas utilizando la API [ExchangeRate](https://v6.exchangerate-api.com/v6/). El programa se ejecuta en la terminal y está desarrollado con **Maven**, lo que facilita su administración de dependencias y estructura.
+
+---
+
+## Características
+- Conversión entre múltiples monedas en tiempo real.
+- Consumo de datos desde la API ExchangeRate.
+- Manejo de errores para fallos en la conexión o solicitudes inválidas.
+- Diseño modular y orientado a objetos.
+
+---
+
+## Requisitos
+- **Java 16** o superior.
+- **Maven** instalado en tu sistema.
+- Una cuenta en [ExchangeRate](https://www.exchangerate-api.com/) para obtener una clave API.
+
+---
+
+## Instalación y Ejecución
+
+1. Clona este repositorio:
+   ```bash
+   git clone https://github.com/jmanriquecha/conversor-de-moneda.git
+   cd conversor-moneda-terminal
+2. Configura tu clave API en el archivo ConsultaMoneda:
+   ```java
+   key = "TU_CLAVE_API";
+3. Compila el proyecto con Maven:
+   ```bash
+   mvn clean install
+4. Ejecuta el programa:
+   ```bash
+   java -jar target/conversor-moneda-1.0.jar
+
+---   
+
+## Uso
+1. Al iniciar, el programa solicitará:
+   * Cantidad a convertir.
+   * Moneda de origen (código ISO, por ejemplo, USD).
+   * Moneda de destino (código ISO, por ejemplo, EUR).
+2. El resultado se mostrará en tiempo real con los datos obtenidos de la API.
+
+---
+
+## Tecnologías Utilizadas
+   * Java
+   * Maven
+   * HTTPClient para el consumo de la API.
+
+---
+
+## Autor
+   * Jorge Manrique Chavarro
+   * [LinkedIn](https://www.linkedin.com/in/jmanriquecha/) | [GitHub](https://github.com/jmanriquecha)
+
+---
+## Licencia
+Este proyecto está bajo la licencia MIT. Para más detalles, consulta el archivo [LICENSE](https://github.com/jmanriquecha/conversor-de-moneda?tab=MIT-1-ov-file).
--- src/main/java/com/jmanrique/conversor_de_moneda/principal/App.java
+++ src/main/java/com/jmanrique/conversor_de_moneda/principal/App.java
@@ -1,65 +1,48 @@
 package com.jmanrique.conversor_de_moneda.principal;
 
-import com.jmanrique.conversor_de_moneda.modelos.ConsultaMoneda;
 import com.jmanrique.conversor_de_moneda.modelos.ConversionMoneda;
-import com.jmanrique.conversor_de_moneda.modelos.Moneda;
 
-import java.text.DecimalFormat;
 import java.util.Scanner;
 
 public class App {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         ConversionMoneda valor = new ConversionMoneda();
-        var mensaje = "Ingresa el valor que deseas convertir";
-        double cantidad = 0;
-        var monedaBase = "";
-        var convertirA = "";
+        double cantidad;
+        String monedaBase;
+        String convertirA;
+        var salir = "Elegiste salir del sistema...";
 
         var menu = """
                 
-                **************************************************
-                Menú principal del Conversor de Moneda
+                *****************************************************
+                --> Bienvenid@ al sistema de conversion de moneda <--
                 
-                1). Dolar Estaunidense ==> Peso Colombiano
-                2). Peso Colombiano    ==> Dolar Estaunidense
-                3). Salir
+                Por favor ingrese la moneda origen y destino (ej. USD) y el monto.
+                >> Para salir ingrese -1 en el monto <<
                 
-                Elija una opción
-                ***************************************************
+                ******************************************************
                 """;
 
         // menú
-        var flag = false;
         try {
-            while (!flag){
+            while (true){
                 System.out.println(menu);
-                var opcionMoneda = Integer.parseInt(scanner.nextLine());
-                if (opcionMoneda == 3){
-                    flag = true;
+                System.out.print("Ingrese el monto: ");
+                cantidad = Double.parseDouble(scanner.nextLine());
+                if(cantidad == -1){
+                    System.out.println(salir);
+                    return;
                 }
+                System.out.print("Ingrese la moneda de origen: ");
+                monedaBase = scanner.nextLine().toUpperCase().strip();
 
-                switch (opcionMoneda){
-                    case 1:
-                        monedaBase = "USD";
-                        convertirA = "COP";
-                        System.out.println(mensaje);
-                        cantidad = Double.parseDouble(scanner.nextLine());
-                        valor.valorAConvertir(cantidad,monedaBase, convertirA);
-                        break;
-                    case 2:
-                        monedaBase = "COP";
-                        convertirA = "USD";
-                        System.out.println(mensaje);
-                        cantidad = Double.parseDouble(scanner.nextLine());
-                        valor.valorAConvertir(cantidad,monedaBase, convertirA);
-                        break;
-                    default:
-                        System.out.println("Saliendo del sistema!");
-                        break;
-                }
+                System.out.print("Ingrese la moneda destino: ");
+                convertirA = scanner.nextLine().toUpperCase().strip();
 
+                valor.valorAConvertir(cantidad,monedaBase, convertirA);
             }
+
         }catch (Exception e){
             System.out.println("Se presento un error en la conversión: " + e.getMessage());
         }
